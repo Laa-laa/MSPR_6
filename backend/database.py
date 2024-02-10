8e660c414be2e79ebc3f6f4e97db74e9d3dce1c7
m-ornella <135030958+m-ornella@users.noreply.github.com>
2024-02-10 18:17:30 +0100

    Update database.py - testing dataset printing code

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+from models import *
 
 engine = create_engine("sqlite:///DB_MSPR_6.db")
 
@@ -12,3 +12,10 @@ session = Session()
 # Declarative Mapping
 Base = declarative_base()
 Base.metadata.create_all(engine)
+
+
+# Code for printing dataset
+
+# all_messages = session.query(Message).all()
+# for message in all_messages:
+#     print(f"ID: {message.id}, Content: {message.content}")
