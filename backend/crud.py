77e1c0a5154dfed8ac6cd7d9102bdd8ca01f40a4
m-ornella <marieornella.vitry@gmail.com>
2024-02-15 19:21:57 +0100

    merge fixed

--- backend/crud.py
+++ backend/crud.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from sqlalchemy.orm import Session
 from . import models, schemas
 
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
-=======
-from sqlalchemy.orm import Session
-from . import models, schemas
-
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
->>>>>>> 5c9fb5d534aa02da0ae3cbabf5908cd34b59c804
--- backend/schemas.py
+++ backend/schemas.py
@@ -2,11 +2,6 @@ from pydantic import BaseModel
 
 
 # Pydantic models
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 5c9fb5d534aa02da0ae3cbabf5908cd34b59c804
 class User(BaseModel):
     id: int
     name: str | None = None
@@ -16,10 +11,7 @@ class User(BaseModel):
     isBotanist: bool
     birthday: str
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 5c9fb5d534aa02da0ae3cbabf5908cd34b59c804
 class Message(BaseModel):
     id: int
     idSender: int
