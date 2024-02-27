2f4c0014d6c5277f47f7c6626103e0f5b1e2bb3d
m-ornella <marieornella.vitry@gmail.com>
2024-02-27 17:29:16 +0100

    tailwind + register component inputs

--- project/src/components/Register.jsx
+++ project/src/components/Register.jsx
@@ -1,12 +1,24 @@
 // Register.jsx
-import React from 'react';
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
 
 const Register = ({ onClick }) => {
+  const [value, setValue] = useState('');
   return (
-    <div className="registerContainer">
-      <h2 className="title">Inscription</h2>
+    <div className="max-w-md mx-auto p-6 bg-white rounded-lg justify-center">
+      <h2 className="text-2xl font-semibold mb-6">Inscription</h2>
+
+      <div className="flex flex-col space-y-4">
+            <InputText value={value} onChange={(e) => setValue(e.target.value) } placeholder="Nom" className="border border-secondary rounded-full p-2 h-12"/>
+            <InputText value={value} onChange={(e) => setValue(e.target.value)} placeholder="Prénom" className="border border-secondary rounded-full p-2 h-12" />
+            <InputText value={value} onChange={(e) => setValue(e.target.value)} placeholder="e-mail" className="border border-secondary rounded-full p-2 h-12"/>
+            <InputText value={value} onChange={(e) => setValue(e.target.value)} placeholder="mot de passe" className="border border-secondary rounded-full p-2 h-12"/>
+            <Button label="Submit" className="bg-color-secondary text-white rounded-full p-2 hover:bg-green-600" />
+            <button className="backButton" onClick={onClick}>Retour</button>
+        </div>
       {/* Your registration form elements can go here */}
-      <button className="backButton" onClick={onClick}>Retour</button>
+      
     </div>
   );
 };
---
+++ project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#355e3b",
+      },
+    },
+  },
+  plugins: [],
+};
