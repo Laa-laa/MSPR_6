efb4f944f58b6e4bd59451d50a5f1858741d13ab
m-ornella <marieornella.vitry@gmail.com>
2024-04-01 11:14:03 +0200

    plants questions list fixed

--- project/src/components/PlantQuestionsList.tsx
+++ project/src/components/PlantQuestionsList.tsx
@@ -26,16 +26,16 @@ export default function PlantQuestionsList() {
     return (
         <div className="container p-4"> 
             <h2 className="container p-4">Dernières questions posées par nos plant lovers</h2>
-            <div className="card flex justify-center" style={{ maxHeight: '500px', borderRadius: '15px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', overflow: 'hidden' }}>
+            <div className="card flex flex-col justify-center">
                 {questions.map((question) => (
-                    <Card key={question.Id} className="md:w-25rem" style={{ borderRadius: '15px', maxHeight: '100%' }}>
+                    <Card key={question.Id} className="md:w-25rem mb-4" style={{ borderRadius: '15px', maxHeight: '100%', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', overflow: 'hidden'  }}>
                         <div className="flex">
                             <div className="w-1/3 overflow-hidden" style={{ borderRadius: '15px 0 0 15px' }}>
                                 <img src={process.env.PUBLIC_URL + '/pexels-scott-webb-1022922.jpg'} alt="plant" className="w-full h-full object-cover" style={{ minWidth: '100%' }} />
                             </div>
                             <div className="w-2/3 flex flex-col justify-between flex-grow p-2 justify-between bg-customGreen" style={{ borderRadius: '0 15px 15px 0' }}>
                                 <h3 className='text-white'>{question.Title}</h3>
-                                <p className='text-white'>{question.Content}</p>
+                                <p className='text-white'>{question.Content.slice(0, 60)}{question.Content.length > 60 ? '...' : ''}</p>
                                 <p className='text-white text-xs'>Date: {question.DateSent}</p>
                             </div>
                         </div>
